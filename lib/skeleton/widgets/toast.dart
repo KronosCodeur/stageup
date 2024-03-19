409c286339bc60265310c98c89c2ac03904c8f1f
KronosCodeur <piraliarris@gmail.com>
2024-03-19 13:47:33 +0000

    toast

---
+++ lib/skeleton/widgets/toast.dart
@@ -0,0 +1,22 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
+import 'package:stage_up/config/theme/theme.dart';
+
+void toast(
+    {required BuildContext context,
+    required String title,
+    required String message,
+    bool success = true}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        title: title,
+        message: message,
+        color: success ? AppTheme.primary : AppTheme.error,
+        contentType: success ? ContentType.success : ContentType.failure,
+      ),
+    ),
+  );
+}
