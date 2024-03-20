474723112c044e1df419fd47133be69310cc5ade
KronosCodeur <piraliarris@gmail.com>
2024-03-20 03:57:53 +0000

    change password page

---
+++ lib/features/security/ui/pages/change_password.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stage_up/features/security/ui/manager/security_manager.dart';
+
+class ChangePasswordPage extends StatelessWidget {
+  const ChangePasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SecurityManager>(builder: (context, security, child) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Change Password Page"),
+        ),
+      );
+    });
+  }
+}
