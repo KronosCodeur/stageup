87474b0185c1eb5a4f016b6a6ed89bcac19c613a
KronosCodeur <piraliarris@gmail.com>
2024-03-20 09:03:14 +0000

    number verification page

---
+++ lib/features/security/ui/pages/number_verification_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stage_up/features/security/ui/manager/security_manager.dart';
+
+class NumberVerificationPage extends StatelessWidget {
+  const NumberVerificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SecurityManager>(builder: (context, security, child) {
+      return const Scaffold(
+        body: Center(
+          child: Text("Number Verification page"),
+        ),
+      );
+    });
+  }
+}
