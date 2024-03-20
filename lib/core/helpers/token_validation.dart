58e67f9fb5f0afffda6df7e9688e976c0f329de0
KronosCodeur <piraliarris@gmail.com>
2024-03-20 13:05:23 +0000

    code refractoring

--- lib/core/helpers/token_validation.dart
+++ lib/core/helpers/token_validation.dart
@@ -1,17 +1,5 @@
-import 'package:http/http.dart' as http;
-import 'package:stage_up/core/constants/api_constants.dart';
 import 'package:stage_up/core/helpers/shared_prefs_helper.dart';
 
-Future<bool> checkIfTokenIsValid() async {
+Future<void> checkIfTokenIsValid() async {
   var token = await LocalStorageHelper.getStringData("token");
-  var request = http.Request('GET',
-      Uri.parse("${ApiConstants.apiAuthUrl}/IsTokenValid/Bearer $token"));
-
-  http.StreamedResponse response = await request.send();
-
-  if (response.statusCode == 200) {
-    return true;
-  } else {
-    return false;
-  }
 }
