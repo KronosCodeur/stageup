7f132695a9eae77acb6704e877b53ceabc9c1a37
KronosCodeur <piraliarris@gmail.com>
2024-03-20 13:05:07 +0000

    routing manager

---
+++ lib/skeleton/managers/routing_manager.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:stage_up/features/interships/ui/pages/account_page.dart';
+import 'package:stage_up/features/interships/ui/pages/applied_page.dart';
+import 'package:stage_up/features/interships/ui/pages/home_page.dart';
+import 'package:stage_up/features/interships/ui/pages/notifications_page.dart';
+
+class RoutingManager extends ChangeNotifier {
+  final List<Map<String, dynamic>> pages = [
+    {
+      'title': 'Rechercher, Trouver et Postuler',
+      'name': 'Accueil',
+      'icon': Icons.home,
+      'page': const HomePage(),
+    },
+    {
+      'title': 'Demandes',
+      'icon': Icons.assignment_ind,
+      'name': 'Demandes',
+      'page': const AppliedPage(),
+    },
+    {
+      'title': 'Notifications',
+      'icon': Icons.notifications,
+      'name': 'Notifications',
+      'page': const NotificationsPage(),
+    },
+    {
+      'title': 'Mon Compte',
+      'name': 'Compte',
+      'icon': Icons.person,
+      'page': const AccountPage(),
+    }
+  ];
+  int currentPage = 0;
+  setCurrentPage(int index) {
+    currentPage = index;
+    notifyListeners();
+  }
+
+  Widget getCurrentPage() {
+    return pages[currentPage]['page'];
+  }
+
+  String getCurrentTitle() {
+    return pages[currentPage]['title'];
+  }
+
+  IconData getCurrentIcon() {
+    return pages[currentPage]['icon'];
+  }
+
+  String getCurrentName() {
+    return pages[currentPage]['name'];
+  }
+}
