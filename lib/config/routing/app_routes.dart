f7228450465f75223827af167139b5ef3a810dff
KronosCodeur <piraliarris@gmail.com>
2024-03-20 03:51:40 +0000

    app routes

---
+++ lib/config/routing/app_routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  static const String onBoarding = 'OnBoarding';
+  static const String splashRoute = 'Splash';
+  static const String loginRoute = 'Login';
+  static const String registerRoute = 'Register';
+  static const String otpValidationRoute = 'OTPValidation';
+  static const String homeRoute = 'Home';
+  static const String rootRoute = 'Root';
+  static const String profileRoute = 'Profile';
+  static const String internshipInfo = 'InternshipInfo';
+}
