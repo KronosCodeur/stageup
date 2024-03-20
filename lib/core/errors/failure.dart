6867b279448cce9c2ba95b7fe3afd5331b7d1fcd
KronosCodeur <piraliarris@gmail.com>
2024-03-20 03:58:53 +0000

    failure

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,16 @@
+abstract class Failure {
+  final String errorMessage;
+  const Failure({required this.errorMessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errorMessage});
+}
+
+class CacheFailure extends Failure {
+  CacheFailure({required super.errorMessage});
+}
+
+class TestingFailure extends Failure {
+  TestingFailure({required super.errorMessage});
+}
