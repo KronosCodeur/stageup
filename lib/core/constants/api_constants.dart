618df62ff47784855deba3690c49f32777c13200
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:18:29 +0000

    api endpoints

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,3 +1,13 @@
 class ApiConstants {
-  static const String apiBaseUrl = "https://jpoapp.onrender.com";
+  static const String _apiBaseUrl = "https://stageup-api.dracarys.tech/api/v1";
+  static const String loginEndpoint = "$_apiBaseUrl/UserAuth/Login";
+  static const String registerEndpoint = "$_apiBaseUrl/UserAuth/Register";
+  static const String uploadProfileEndpoint = "$_apiBaseUrl/Image/Upload";
+  static const String profileBaseUrl = "$_apiBaseUrl/Image/Download?filename=";
+  static const String getAllInternshipsEndpoint =
+      "$_apiBaseUrl/Internship/GetAllPublished";
+  static const String getAllDomainsEndpoint = "$_apiBaseUrl/Domain/GetAll";
+  static const String getAllOptionsEndpoint = "$_apiBaseUrl/Option/GetAll";
+  static const String getUserApplicationsEndpoint =
+      "$_apiBaseUrl/Application/GetUserApplications";
 }
