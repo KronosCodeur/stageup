f9f9315df6bd8256e86337bb17b6b50cb886a80a
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:58:20 +0000

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stage_up/config/routing/app_routing.dart';
-import 'package:stage_up/config/theme/theme.dart';
-import 'package:stage_up/features/interships/ui/manager/intership_manager.dart';
-import 'package:stage_up/features/security/ui/manager/security_manager.dart';
-import 'package:stage_up/skeleton/managers/on_boarding_manager.dart';
-import 'package:stage_up/skeleton/managers/routing_manager.dart';
+import 'package:stageup/config/routing/app_routing.dart';
+import 'package:stageup/config/theme/theme.dart';
+import 'package:stageup/features/applications/ui/manager/application_manager.dart';
+import 'package:stageup/features/interships/ui/manager/intership_manager.dart';
+import 'package:stageup/features/security/ui/manager/register_step_manager.dart';
+import 'package:stageup/features/security/ui/manager/security_manager.dart';
+import 'package:stageup/skeleton/managers/on_boarding_manager.dart';
+import 'package:stageup/skeleton/managers/routing_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => SecurityManager()),
-      ChangeNotifierProvider(create: (_) => OnBoardingManager()),
-      ChangeNotifierProvider(create: (_) => RoutingManager()),
-      ChangeNotifierProvider(create: (_) => InternshipManager()),
-    ], child: const StageUp()),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SecurityManager()),
+        ChangeNotifierProvider(create: (_) => OnBoardingManager()),
+        ChangeNotifierProvider(create: (_) => RoutingManager()),
+        ChangeNotifierProvider(create: (_) => InternshipManager()),
+        ChangeNotifierProvider(create: (_) => RegisterStepManager()),
+        ChangeNotifierProvider(create: (_) => ApplicationManager()),
+      ],
+      child: const StageUp(),
+    ),
   );
 }
 
