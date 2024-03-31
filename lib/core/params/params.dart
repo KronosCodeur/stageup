a9532e91d352b52f171cd28916d185f4bfc18255
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:00:01 +0000

    params

--- lib/core/params/params.dart
+++ lib/core/params/params.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 class NoParams {}
 
 class TemplateParams {}
 
 class ParamsLogin {
-  final String phone;
-  final String password;
+  String phone;
+  String password;
 
   Map<String, String> toJson() => {
         'phone': phone,
@@ -14,20 +16,67 @@ class ParamsLogin {
   ParamsLogin({required this.phone, required this.password});
 }
 
+class ParamsRegister {
+  String phone;
+  String password;
+  String firstname;
+  String lastname;
+  int gender;
+  String address;
+  String dateOfBirth;
+  String school;
+  String profile;
+
+  ParamsRegister({
+    required this.phone,
+    required this.password,
+    required this.firstname,
+    required this.lastname,
+    required this.gender,
+    required this.address,
+    required this.dateOfBirth,
+    required this.school,
+    required this.profile,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'phone': phone,
+        'password': password,
+        'firstname': firstname,
+        'lastname': lastname,
+        'gender': gender,
+        'address': address,
+        'dateOfBirth': DateTime.parse(dateOfBirth).toIso8601String(),
+        'school': school,
+        'profile': profile,
+      };
+}
+
 class ParamOne {
   String id;
 
   ParamOne({required this.id});
 }
 
-class ParamsBooking {
-  final String travelId;
-  final String userId;
+class UploadProfileParam {
+  File file;
+  String filename;
 
-  Map<String, String> toJson() => {
-        'travelId': travelId,
-        'userId': userId,
-      };
+  UploadProfileParam({required this.file, required this.filename});
+}
 
-  ParamsBooking({required this.travelId, required this.userId});
+class ApplyParams {
+  String internshipId;
+  String internId;
+  File cv;
+  File? motivation;
+  File? recommendation;
+
+  ApplyParams({
+    required this.internshipId,
+    required this.internId,
+    required this.cv,
+    this.motivation,
+    this.recommendation,
+  });
 }
