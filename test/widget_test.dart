88a4f921f9132501bfe6053aa9c328c77e1c892a
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:05:59 +0000

    test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stage_up/main.dart';
+import 'package:stageup/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
