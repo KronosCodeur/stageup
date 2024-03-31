e8cef42f9224e8f3be717320cfd4d7ddd294790e
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:05:49 +0000

    domain model

--- lib/features/interships/data/models/domain_model.dart
+++ lib/features/interships/data/models/domain_model.dart
@@ -1,30 +1,25 @@
-import 'package:stage_up/features/interships/data/models/option_model.dart';
-
 class DomainModel {
-  int id;
-  String title;
-  List<OptionModel> options;
-
   DomainModel({
     required this.id,
     required this.title,
-    required this.options,
+    required this.description,
   });
+
+  String id;
+  String title;
+  String description;
 
   factory DomainModel.fromJson(Map<String, dynamic> json) {
     return DomainModel(
       id: json['id'],
       title: json['title'],
-      options: json['options'] == null
-          ? List<OptionModel>.from([])
-          : List<OptionModel>.from(
-              json['options'].map((x) => OptionModel.fromJson(x))),
+      description: json['description'],
     );
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'title': title,
-        'options': options.map((e) => e.toJson()).toList(),
+        'description': description,
       };
 }
