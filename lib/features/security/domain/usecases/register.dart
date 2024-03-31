09b04621718b8eded51fa11b39827679a93e041e
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:00:15 +0000

    register usecase

--- lib/features/security/domain/usecases/register.dart
+++ lib/features/security/domain/usecases/register.dart
@@ -1,1 +1,17 @@
-class Register {}
+import 'package:dartz/dartz.dart';
+import 'package:stageup/core/errors/failure.dart';
+import 'package:stageup/core/params/params.dart';
+import 'package:stageup/core/usecases/usecase.dart';
+import 'package:stageup/features/security/domain/repositories/security_repository.dart';
+import 'package:stageup/skeleton/Dto/request_response.dart';
+
+class Register implements UseCase<RequestResponse, ParamsRegister> {
+  final SecurityRepository securityRepository;
+
+  Register({required this.securityRepository});
+
+  @override
+  Future<Either<Failure, RequestResponse>> call(ParamsRegister params) async {
+    return await securityRepository.register(params);
+  }
+}
