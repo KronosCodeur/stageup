4315364c245274a79357bfada756f4de388355a4
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:58:04 +0000

    internship manager

--- lib/features/interships/ui/manager/intership_manager.dart
+++ lib/features/interships/ui/manager/intership_manager.dart
@@ -1,36 +1,52 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:flutter/material.dart';
-import 'package:stage_up/features/interships/data/models/company_model.dart';
-import 'package:stage_up/features/interships/data/models/intership_model.dart';
+import 'package:stageup/core/connection/network_info.dart';
+import 'package:stageup/core/errors/failure.dart';
+import 'package:stageup/core/params/params.dart';
+import 'package:stageup/features/interships/data/services/internship_service.dart';
+import 'package:stageup/features/interships/data/sources/local/internships_local_source.dart';
+import 'package:stageup/features/interships/data/sources/remote/internships_remote_source.dart';
+import 'package:stageup/features/interships/domain/entities/intership.dart';
+import 'package:stageup/features/interships/domain/usecases/get_all_internships.dart';
 
 class InternshipManager extends ChangeNotifier {
   String searchText = "";
-  InternshipModel? selectedInternship;
-  List<InternshipModel> internships = [
-    InternshipModel(
-      id: 1,
-      title: "Dévéloppeur d'Application Mobile",
-      description:
-          "Nous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs deNous recherchons un develepeurs de",
-      startDate: DateTime.now(),
-      endDate: DateTime.now(),
-      places: 5,
-      company: CompanyModel(
-        id: 1,
-        name: "Dracarys Technology",
-        logo: "logo",
-        email: "contact@dracarys.tech",
-        location: "Legbassito,Lomé",
-      ),
-    ),
-  ];
+  Failure? failure;
+  Internship? selectedInternship;
+  List<Internship>? internships;
 
   setSearchText(String value) {
     searchText = value;
     notifyListeners();
   }
 
-  void setSelectedInternship(InternshipModel internship) {
+  void setSelectedInternship(Internship internship) {
     selectedInternship = internship;
+    notifyListeners();
+  }
+
+  Future<void> fetchInternships() async {
+    failure = null;
+    internships = null;
+    DataConnectionChecker dataConnectionChecker = DataConnectionChecker();
+    INetworkInfo iNetworkInfo = NetworkInfo(dataConnectionChecker);
+    InternshipLocalSource internshipLocalSource = InternshipLocalSource();
+    InternshipsRemoteSource internshipRemoteSource = InternshipsRemoteSource();
+    InternshipService internshipService = InternshipService(
+      iNetworkInfo: iNetworkInfo,
+      internshipLocalSource: internshipLocalSource,
+      internshipsRemoteSource: internshipRemoteSource,
+    );
+    GetAllInternships getAllInternships =
+        GetAllInternships(internshipRepository: internshipService);
+    var result = await getAllInternships.call(NoParams());
+    result.fold((fail) {
+      failure = fail;
+      notifyListeners();
+    }, (data) {
+      internships = data.map((e) => Internship.fromJson(e.toJson())).toList();
+      notifyListeners();
+    });
     notifyListeners();
   }
 }
