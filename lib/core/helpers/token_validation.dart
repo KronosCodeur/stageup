0fc26caaf16f52cb1a97c4452ba68fb79ebf641c
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:03:59 +0000

    token validation

--- lib/core/helpers/token_validation.dart
+++ lib/core/helpers/token_validation.dart
@@ -1,4 +1,4 @@
-import 'package:stage_up/core/helpers/shared_prefs_helper.dart';
+import 'package:stageup/core/helpers/shared_prefs_helper.dart';
 
 Future<void> checkIfTokenIsValid() async {
   var token = await LocalStorageHelper.getStringData("token");
