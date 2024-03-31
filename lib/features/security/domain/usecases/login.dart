92e5c5bc9c8427a9b8949befc538f96842805b63
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:00:22 +0000

    login usecase

--- lib/features/security/domain/usecases/login.dart
+++ lib/features/security/domain/usecases/login.dart
@@ -1,1 +1,17 @@
-class Login {}
+import 'package:dartz/dartz.dart';
+import 'package:stageup/core/errors/failure.dart';
+import 'package:stageup/core/params/params.dart';
+import 'package:stageup/core/usecases/usecase.dart';
+import 'package:stageup/features/security/data/models/user_model.dart';
+import 'package:stageup/features/security/domain/repositories/security_repository.dart';
+
+class Login implements UseCase<UserModel, ParamsLogin> {
+  final SecurityRepository securityRepository;
+
+  Login({required this.securityRepository});
+
+  @override
+  Future<Either<Failure, UserModel>> call(ParamsLogin params) async {
+    return await securityRepository.login(params);
+  }
+}
