11d3ef464c96b0e301a203142360d9f35bf12ab4
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:04:26 +0000

    security repository

--- lib/features/security/domain/repositories/security_repository.dart
+++ lib/features/security/domain/repositories/security_repository.dart
@@ -1,8 +1,15 @@
 import 'package:dartz/dartz.dart';
-import 'package:stage_up/core/errors/failure.dart';
-import 'package:stage_up/core/params/params.dart';
-import 'package:stage_up/features/security/domain/entities/student.dart';
+import 'package:stageup/core/errors/failure.dart';
+import 'package:stageup/core/params/params.dart';
+import 'package:stageup/features/security/data/models/user_model.dart';
+import 'package:stageup/skeleton/Dto/request_response.dart';
 
 abstract class SecurityRepository {
-  Future<Either<Failure, Student>> login(ParamsLogin paramsLogin);
+  Future<Either<Failure, UserModel>> login(ParamsLogin paramsLogin);
+
+  Future<Either<Failure, RequestResponse>> register(
+      ParamsRegister paramsRegister);
+
+  Future<Either<Failure, RequestResponse>> uploadProfile(
+      UploadProfileParam param);
 }
