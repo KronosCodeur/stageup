2a71acc139a9cd3adfc987bb7428e0de568710d8
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:20:53 +0000

    splash page

--- lib/skeleton/pages/splash_page.dart
+++ lib/skeleton/pages/splash_page.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:stage_up/config/routing/app_routes.dart';
 import 'package:stage_up/config/theme/theme.dart';
+import 'package:stage_up/core/helpers/shared_prefs_helper.dart';
+import 'package:stage_up/features/applications/ui/manager/application_manager.dart';
+import 'package:stage_up/features/interships/ui/manager/intership_manager.dart';
+import 'package:stage_up/features/security/domain/entities/user.dart';
+import 'package:stage_up/features/security/ui/manager/security_manager.dart';
 import 'package:stage_up/skeleton/widgets/loading.dart';
 
 class SplashPage extends StatefulWidget {
@@ -15,13 +23,58 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Future.delayed(
-      const Duration(seconds: 2),
-    ).whenComplete(() {
-      GoRouter.of(context).pushReplacementNamed(AppRoutes.onBoarding);
+    Future.delayed(const Duration(seconds: 1)).whenComplete(() {
+      /*checkNewUser().then((value) {
+        if (value) {
+          GoRouter.of(context).pushReplacementNamed(AppRoutes.onBoarding);
+        } else {*/
+      getUser().then((value) {
+        if (value != null) {
+          Provider.of<SecurityManager>(context, listen: false).user = value;
+          Provider.of<InternshipManager>(context, listen: false)
+              .fetchInternships()
+              .whenComplete(() {
+            Provider.of<ApplicationManager>(context, listen: false)
+                .fetchApplications()
+                .whenComplete(
+                  () => GoRouter.of(context)
+                      .pushReplacementNamed(AppRoutes.rootRoute),
+                );
+          });
+        } else {
+          GoRouter.of(context).pushReplacementNamed(AppRoutes.onBoarding);
+        }
+      });
+      /* }
+      });*/
     });
     super.initState();
   }
+
+  Future<User?> getUser() async {
+    var token = await LocalStorageHelper.getStringData('token');
+    if (token != null) {
+      var userString = await LocalStorageHelper.getStringData('user');
+      if (userString != null) {
+        var user = User.fromJson(jsonDecode(userString));
+        return user;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  /* Future<bool> checkNewUser() async {
+    var isNewUser = await LocalStorageHelper.getBoolData('isNewUser');
+    if (isNewUser == null) {
+      return true;
+    } else {
+      await LocalStorageHelper.addBoolData('isNewUser', true);
+      return false;
+    }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +85,7 @@ class _SplashPageState extends State<SplashPage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Gap(size.height * 0.2),
-            Text(
+            const Text(
               "Stage UP",
               style: TextStyle(
                 color: AppTheme.primary,
@@ -41,7 +94,7 @@ class _SplashPageState extends State<SplashPage> {
               ),
             ),
             Gap(size.height * 0.1),
-            Loading(),
+            const Loading(),
           ],
         ),
       ),
