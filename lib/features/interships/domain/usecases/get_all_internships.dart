77b14b0bf6aacb8c8bba03a808c6def8b54dc75c
KronosCodeur <piraliarris@gmail.com>
2024-03-31 06:00:38 +0000

    getAllInternships usecase

--- lib/features/interships/domain/usecases/get_all_internships.dart
+++ lib/features/interships/domain/usecases/get_all_internships.dart
@@ -1,1 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:stageup/core/errors/failure.dart';
+import 'package:stageup/core/params/params.dart';
+import 'package:stageup/core/usecases/usecase.dart';
+import 'package:stageup/features/interships/data/models/intership_model.dart';
+import 'package:stageup/features/interships/domain/repositories/internships_repository.dart';
 
+class GetAllInternships implements UseCase<List<InternshipModel>, NoParams> {
+  final InternshipRepository internshipRepository;
+
+  GetAllInternships({
+    required this.internshipRepository,
+  });
+
+  @override
+  Future<Either<Failure, List<InternshipModel>>> call(NoParams params) async {
+    return await internshipRepository.getAll(params);
+  }
+}
