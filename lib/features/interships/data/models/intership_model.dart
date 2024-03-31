49287833b032611ecf778bce7b2742ccc021addc
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:57:56 +0000

    internship model

--- lib/features/interships/data/models/intership_model.dart
+++ lib/features/interships/data/models/intership_model.dart
@@ -1,43 +1,71 @@
-import 'package:stage_up/features/interships/data/models/company_model.dart';
+import 'package:stageup/features/interships/data/models/company_model.dart';
+import 'package:stageup/features/interships/data/models/option_model.dart';
 
 class InternshipModel {
-  int id;
-  String title;
-  String description;
-  DateTime startDate;
-  DateTime endDate;
-  int places;
-  CompanyModel company;
-
   InternshipModel({
     required this.id,
     required this.title,
     required this.description,
-    required this.startDate,
-    required this.endDate,
-    required this.places,
+    required this.skills,
+    required this.responsibilities,
     required this.company,
+    required this.option,
+    required this.startAt,
+    required this.endAt,
+    required this.createdAt,
+    required this.updateAt,
+    required this.published,
+    required this.publishedAt,
+    required this.numbersOfApplications,
   });
+
+  String id;
+  String title;
+  String description;
+  String skills;
+  String responsibilities;
+  CompanyModel company;
+  OptionModel option;
+  String startAt;
+  String endAt;
+  String createdAt;
+  String updateAt;
+  bool published;
+  String publishedAt;
+  int numbersOfApplications;
 
   factory InternshipModel.fromJson(Map<String, dynamic> json) {
     return InternshipModel(
-      id: json['id'],
-      title: json['title'],
-      description: json['description'],
-      startDate: json['startDate'],
-      endDate: json['endDate'],
-      places: json['places'],
-      company: CompanyModel.fromJson(json['company']),
-    );
+        id: json['id'],
+        title: json['title'],
+        description: json['description'],
+        skills: json['skills'],
+        responsibilities: json['responsibilities'],
+        company: CompanyModel.fromJson(json['company']),
+        option: OptionModel.fromJson(json['option']),
+        startAt: json['startAt'],
+        endAt: json['endAt'],
+        createdAt: json['createdAt'],
+        updateAt: json['updateAt'],
+        published: json['published'],
+        publishedAt: json['publishedAt'],
+        numbersOfApplications: json['numbersOfApplications']);
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'title': title,
         'description': description,
-        'startDate': startDate,
-        'endDate': endDate,
-        'places': places,
+        'skills': skills,
+        'responsibilities': responsibilities,
         'company': company.toJson(),
+        'option': option.toJson(),
+        'startAt': startAt,
+        'endAt': endAt,
+        'createdAt': createdAt,
+        'updateAt': updateAt,
+        'published': published,
+        'publishedAt': publishedAt,
+        'numbersOfApplications': numbersOfApplications,
       };
 }
