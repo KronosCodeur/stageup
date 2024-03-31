49644fcea60efebf4166553abd1562c05781faed
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:57:44 +0000

    company model

--- lib/features/interships/data/models/company_model.dart
+++ lib/features/interships/data/models/company_model.dart
@@ -1,32 +1,47 @@
-class CompanyModel {
-  int id;
-  String name;
-  String logo;
-  String email;
-  String location;
+import 'package:stageup/core/constants/api_constants.dart';
 
-  CompanyModel(
-      {required this.id,
-      required this.name,
-      required this.logo,
-      required this.email,
-      required this.location});
+class CompanyModel {
+  CompanyModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.logo,
+    required this.address,
+    required this.email,
+    required this.verified,
+    required this.contact,
+  });
+
+  String id;
+  String name;
+  String description;
+  String logo;
+  String address;
+  String email;
+  bool verified;
+  String contact;
 
   factory CompanyModel.fromJson(Map<String, dynamic> json) {
     return CompanyModel(
       id: json['id'],
       name: json['name'],
-      logo: json['logo'],
+      description: json['description'],
+      logo: ApiConstants.profileBaseUrl + json['logo'],
+      address: json['address'],
       email: json['email'],
-      location: json['location'],
+      verified: json['verified'],
+      contact: json['contact'],
     );
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
+        'description': description,
         'logo': logo,
+        'address': address,
         'email': email,
-        'location': location,
+        'verified': verified,
+        'contact': contact,
       };
 }
