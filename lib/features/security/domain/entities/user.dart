c931a8bdd7a7c3e04edcd08419ed14be3aff5665
KronosCodeur <piraliarris@gmail.com>
2024-03-31 05:19:12 +0000

    user entity

---
+++ lib/features/security/domain/entities/user.dart
@@ -0,0 +1,48 @@
+class User {
+  User({
+    required this.id,
+    required this.firstname,
+    required this.lastname,
+    required this.gender,
+    required this.address,
+    required this.phone,
+    required this.dateOfBirth,
+    required this.school,
+    required this.profile,
+  });
+  String id;
+  String firstname;
+  String lastname;
+  String gender;
+  String address;
+  String phone;
+  String dateOfBirth;
+  String school;
+  String profile;
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      firstname: json['firstname'],
+      lastname: json['lastname'],
+      gender: json['gender'],
+      address: json['address'],
+      phone: json['phone'],
+      dateOfBirth: json['dateOfBirth'],
+      school: json['school'],
+      profile: json['profile'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'firstname': firstname,
+        'lastname': lastname,
+        'gender': gender,
+        'address': address,
+        'phone': phone,
+        'dateOfBirth': dateOfBirth,
+        'school': school,
+        'profile': profile,
+      };
+}
